c9dc6c28374fdcd963190271ad8a5450c3269bdc
ShreyasMahesh <Shreyashosahalli2010@gmail.com>
2024-05-12 19:26:36 +0300

    Creating the models!!!

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import os
 from keras._tf_keras.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.api.layers import Conv2D
 
+from tensorflow.python.keras import models, layers 
 # Load the dataset
 execution_path = os.getcwd()
 data_directory = os.path.join(execution_path, 'idenprof')
 
+data_test_directory = os.path.join(data_directory, 'test')
+
+
 # Decide on the models
 model_name = 'idenprof_model-{epoch:03d}-{val_acc}.weights.h5'
 model_dir = os.path.join(execution_path, 'idenprof_models')
+
+idenprof = os.listdir(data_test_directory)
+
+# print("idenprof",len(idenprof))
 
 if not os.path.isdir(model_dir):
     os.makedirs(model_dir)
@@ -41,4 +50,42 @@ def lr_schedule(epoch):
 
 lr_schedule = LearningRateScheduler(lr_schedule)
 
-print(lr_schedule)
+# print(lr_schedule)
+# Building the model
+# Cov2D()->Filters in a model
+# Level of Cov2D filters: 32, 64, 128
+def resnet_model(input, channel_depth):
+    Conv2D(level=32,filters=channel_depth, kernel_size=200, padding="same")
+    Conv2D(level=64,filters=channel_depth, kernel_size=200, padding="same")
+    Conv2D(level=128,filters=channel_depth, kernel_size=200, padding="same")
+
+    strided_pool = 0
+    
+    for i in idenprof:
+        strided_pool += i
+
+    return strided_pool
+
+
+# Model creation
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(100, 100)),
+    layers.Flatten(),
+    layers.Conv2D(64, (3,3), activation='relu', input_shape=(100, 100)),
+    layers.Flatten(),
+    layers.Conv2D(128, (3,3), activation='relu', input_shape=(100, 100)),
+    layers.Flatten(),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(1, activation='sigmoid')])
+
+
+
+
+    
+# Modile Compile
+model.compile(optimizer='adam', loss="percentage", metrics=["accuracy"])
+
+
+# Training the model
+model.fit(idenprof['chef'], idenprof['doctor'], epochs=10, batch_size=200)
+
